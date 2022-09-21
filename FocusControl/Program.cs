f92f117bde249bf29e5fe7d0a3ac768a0183e6dd
Tatiana <k.t.s-92@yandex.ru>
2022-09-21 11:00:02 +0700

    write method Array

---
+++ FocusControl/Program.cs
@@ -0,0 +1,17 @@
+﻿
+
+static string[] FreeArray()
+{
+    string[] arrayM = new string[] {"1" , "12", "365", "3652", "Jan", "Feb", "Marth", "April", "May", "June", "July", "Aug", "Sept", "Oct", "Nov", "December"};
+    string[] array = new string[6];
+    int m;
+    int j = 0;
+    for (int i = 0; i < 6; i++)
+    {
+        m = new Random().Next(0, 15);
+        Console.Write(arrayM[m]);
+        Console.Write(" ");
+    }
+    Console.WriteLine();
+    return array;
+}    
